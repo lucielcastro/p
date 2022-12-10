c54c94ca89d98fafd4e17d3155c844e3da142ac1
Luciel Castro <93848647+lucielcastro@users.noreply.github.com>
2022-12-10 00:15:42 -0300

    ajustes3

--- n2/pessoa_fisica.py
+++ n2/pessoa_fisica.py
@@ -1,8 +1,6 @@
 
-
-alunos = []
 cursos = []
-alunos_curso = {}
+alunos = []
 
 def imprimir_cursos_disponiveis():
    for curso in cursos:
@@ -12,21 +10,53 @@ def imprimir_cursos_disponiveis():
 def verificar_cursos( curso: str):
    for curs in cursos:
       if curso == curs.nome:
-         '''alunos_curso[curs.nome] = dados_aluno'''
+         #alunos_curso[curs.nome] = dados_aluno
          return curs.nome
    print('Curso não cadastrado')
-   encontrar_curso = input('Informe curso:')
+   imprimir_cursos_disponiveis()
+   encontrar_curso = input('Informe um curso:')
    verificar_cursos(encontrar_curso)
    return curs
+   
 
 class Pessoa_fisica:
    def __init__(self, nome: str):
       self.nome=nome
 
+class Aluno(Pessoa_fisica):
+   def __init__(self, nome: str, curso, matricula: str, ano_ingresso: int):
+      super().__init__( nome)
+      self.curso=curso
+      self.matricula=matricula
+      self.ano_ingresso=ano_ingresso
+   
+   def cadastrar_aluno(self, nome, curso, matricula, ano_ingresso):
+      alunos.append(Aluno(nome=nome, curso=curso, matricula=matricula, ano_ingresso=ano_ingresso))
+      print('Aluno cadastrado com sucesso!')
+   
+   def editar_aluno(self, nome):
+      for aluno in alunos:
+         if nome == aluno.nome:
+            print(aluno)
+            print('Qual dados deseja editar?\n\n1-todos')
+            op = int(input('Inorme:'))
+            if op == 1:
+               aluno.nome = input('Nome:')
+               aluno.curso = input('Curso:')
+               aluno.matricula = input('Matricula:')
+               aluno.ano_ingresso = int(input('Ano ingresso:'))
+               print(f'Dados atualizados: {aluno}')
+
+   def imprimir_alunos(self):
+      print('\n**** ALUNOS CADASTRADo ****')
+      for aluno in alunos:
+         print(f'{aluno.nome} {aluno.curso} {aluno.matricula}')
+
 class Curso:
    def __init__(self,nome: str, descricao: str):
       self.nome = nome
       self.descricao = descricao
+      self.alunos = []
    
    def cadastrar_curso(self, nome, descricao):
       cursos.append(Curso(nome=nome, descricao=descricao))
@@ -36,24 +66,17 @@ class Curso:
       print('\n**** CURSOS DISPONIVÉIS ****')
       for curso in cursos:
          print(curso.nome)
-      
-class Aluno(Pessoa_fisica):
-   def __init__(self, nome: str, curso: Curso, matricula: str, ano_ingresso: int):
-      super().__init__( nome)
-      self.matricula=matricula
-      self.ano_ingresso=ano_ingresso
-      self.curso=curso
 
    def matricular_aluno(self, nome, curso, matricula, ano_ingresso):
-      alunos.append(Aluno(nome=nome, curso=curso, matricula=matricula, ano_ingresso=ano_ingresso))
+      self.alunos.append(Aluno(nome=nome, curso=curso, matricula=matricula, ano_ingresso=ano_ingresso))
       print('Aluno matriculado com sucesso!')
 
-   def imprimir_alunos(self):
+   def impri_alunos(self):
       print('\n**** ALUNOS MATRICULADOS ****')
-      for aluno in alunos:
+      for aluno in self.alunos:
          print(aluno.nome)
 
-if not cursos:
+'''if not cursos:
    print('\nATENCAO:\n\nNao ha cursos cadastrados no sistema. Cadastre os cursos que serão ofertados na instituicao.\n')
    op = 1
    while op !=0:
@@ -68,31 +91,47 @@ if not cursos:
          descric = input('Infome descricao: ')
          c = Curso(name, descric)
          c.cadastrar_curso(name, descric)
-         print(f'{c.nome} {c.descricao}')
+         print(f'{c.nome} {c.descricao}')'''
+def v(curso):
+   if len(cursos)==0:
+      print('\nATENCAO:\nNao ha cursos cadastrados no sistema. Cadastre o curso que será ofertado para o aluno.\n')
+      print('\n********** CADASTRAR CURSO **********')
+      name = input('Informe nome: ')
+      descric = input('Infome descricao: ')
+      c = Curso(name, descric)
+      c.cadastrar_curso(name, descric)
+      print(f'{c.nome} {c.descricao}')
+      return c
+   else:
+      verificar_cursos(curso)
 
 op2=1
 while op2 !=0:
-   print('\n##################################')
-   print('#  1-Matricular aluno\n#  2-Consultar Alunos Matriculados\n#  3-Cadastrar Curso\n#  4-Consultar Cursos\n#  0-Sair')
-   print('##################################')
+   print('\n*********** MANTER ALUNO ***********')
+   print('#  1-Cadastrar\n#  2-Editar\n#  3-Deletar\n#  4-Consultar \n#  0-Sair')
+   print('####################################')
    op2=int(input('\nInforme uma das opcoes acima: '))
 
    if op2 == 1:
-      print('\n****** MATRICULAR ALUNO ******')
+      print('\n********** CADASTRAR ALUNO **********')
       name = input('Informe nome: ')
-      imprimir_cursos_disponiveis()
+      #imprimir_cursos_disponiveis()
       curso = input('Informe curso: ')
-      curso_buscado = verificar_cursos(curso)
+      #curso_buscado = verificar_cursos(curso)
+      curso_busca = v(curso)
       mat = input('Infome matricula: ')
       ano_ing = input('Informe ano ingresso: ')
-      a = Aluno(name, curso_buscado, mat, ano_ing)
-      a.matricular_aluno(name, curso_buscado, mat, ano_ing)
-      print(f'Nome: {a.nome}\nCurso: {a.curso}\nMatricula: {a.matricula}\nAno de ingresso: {a.ano_ingresso}')
-   
+      aluno = Aluno(name, curso, mat, ano_ing)
+      aluno.cadastrar_aluno(name, curso, mat, ano_ing)
+      print(f'Nome: {aluno.nome}\nCurso: {aluno.curso}\nMatricula: {aluno.matricula}\nAno de ingresso: {aluno.ano_ingresso}')
    elif op2 == 2:
-      a.imprimir_alunos()
+      nome = input('Informe nome: ')
+      nome.
    
-   elif op2 == 3:
+   elif op2 == 4:
+      aluno.imprimir_alunos()
+   
+   elif op2 == 5:
       print('\n****** CADASTRAR CURSO ******')
       name = input('Informe nome: ')
       descric = input('Infome descricao: ')
@@ -100,8 +139,20 @@ while op2 !=0:
       c.cadastrar_curso(name, descric)
       print(f'{c.nome} {c.descricao}')
    
-   elif op2 == 4:
+   elif op2 == 6:
       c.consultar_cursos()
+
+   '''  if op2 == 1:
+      print('\n****** MATRICULAR ALUNO ******')
+      name = input('Informe nome: ')
+      #imprimir_cursos_disponiveis()
+      curso = input('Informe curso: ')
+      #curso_buscado = verificar_cursos(curso)
+      mat = input('Infome matricula: ')
+      ano_ing = input('Informe ano ingresso: ')
+      aluno = Aluno(name, curso, mat, ano_ing)
+      aluno.matricular_aluno(name, curso, mat, ano_ing)
+      print(f'Nome: {aluno.nome}\nCurso: {aluno.curso}\nMatricula: {aluno.matricula}\nAno de ingresso: {aluno.ano_ingresso}')'''
 
 
 
--- n2/tempCodeRunnerFile.py
+++ n2/tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
- if op == 1:
-      print('\n****** MATRICULAR ALUNO ******')
-      name = input('Informe nome: ')
-      curso = input('Informe curso')
-      mat = input('Infome matricula: ')
-      ano_ing = input('Informe ano ingresso: ')
-      a = Aluno(name, mat , ano_ing)
-      a.matricular_aluno(name,mat, ano_ing)
-      print(f'{a.nome} {a.matricula} {a.ano_ingresso}')
-
-   elif op == 2:
-      a.imprimir_alunos()
+print(f'Nome: {aluno.nome}\nCurso: {aluno.curso}\nMatricula: {aluno.matricula}\nAno de ingresso: {aluno.ano_ingresso}')
